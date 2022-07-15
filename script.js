af1d7b4f10b3f8a45709791e985db1e7b0c5142a
leul <72883746+leulmteka@users.noreply.github.com>
2022-07-15 17:55:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+console.log("running");
+
+const stinger = document.querySelector("#image1")
+stinger.addEventListener("click", () =>{
+
+  location.href = "/weapons/stinger.html"
+})
+
+const spectre = document.querySelector("#image2")
+spectre.addEventListener("click", () =>{
+
+  location.href = "/weapons/spectre.html"
+})
+
+const bucky = document.querySelector("#image3")
+bucky.addEventListener("click", () =>{
+
+  location.href = "/weapons/bucky.html"
+})
+
+const judge = document.querySelector("#image4")
+judge.addEventListener("click", () =>{
+
+  location.href = "/weapons/judge.html"
+})
+
+const bulldog = document.querySelector("#image5")
+bulldog.addEventListener("click", () =>{
+
+  location.href = "/weapons/bulldog.html"
+})
+
+const guardian = document.querySelector("#image6")
+guardian.addEventListener("click", () =>{
+
+  location.href = "/weapons/guardian.html"
+})
+
+const phantom = document.querySelector("#image7")
+phantom.addEventListener("click", () =>{
+
+  location.href = "/weapons/phantom.html"
+})
+
+const vandal = document.querySelector("#image8")
+vandal.addEventListener("click", () =>{
+
+  location.href = "/weapons/vandal.html"
+})
+
+const marshal = document.querySelector("#image9")
+marshal.addEventListener("click", () =>{
+
+  location.href = "/weapons/marshal.html"
+})
+
+const operator = document.querySelector("#image10")
+operator.addEventListener("click", () =>{
+
+  location.href = "/weapons/operator.html"
+})
+
+const ares = document.querySelector("#image11")
+ares.addEventListener("click", () =>{
+
+  location.href = "/weapons/ares.html"
+})
+
+const odin = document.querySelector("#image12")
+odin.addEventListener("click", () =>{
+
+  location.href = "/weapons/odin.html"
+})
